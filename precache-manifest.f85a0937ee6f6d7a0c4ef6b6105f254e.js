e425ffa595581499e3eaf805bee2410c70d01387
Tijani Oluwafemi <olutimedia@gmail.com>
2019-07-22 13:01:23 +0100

    Updates

---
+++ precache-manifest.f85a0937ee6f6d7a0c4ef6b6105f254e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddab165e2e6c61a1bb3f7540fa5e019a",
+    "url": "/myreads/index.html"
+  },
+  {
+    "revision": "252c259e2442a56ba338",
+    "url": "/myreads/static/css/main.4c80d8b6.chunk.css"
+  },
+  {
+    "revision": "0a376eb89cb9ab871bec",
+    "url": "/myreads/static/js/2.6d3cece7.chunk.js"
+  },
+  {
+    "revision": "252c259e2442a56ba338",
+    "url": "/myreads/static/js/main.88e97b14.chunk.js"
+  },
+  {
+    "revision": "ae7976efa8cc24264f0e",
+    "url": "/myreads/static/js/runtime~main.7df25c17.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/myreads/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/myreads/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/myreads/static/media/arrow-drop-down.9d4e3f36.svg"
+  }
+]);
